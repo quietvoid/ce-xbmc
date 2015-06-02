56758fa66dfb5034d18950098a3959a708124720
Sam Nazarko <email@samnazarko.co.uk>
2015-06-02 17:52:40 +0100

    Do not present CEC toasts unless there is an error or library mismatch
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -318,13 +318,6 @@ bool CPeripheralCecAdapter::OpenConnection(void)
   CLog::Log(LOGDEBUG, "{} - opening a connection to the CEC adapter: {}", __FUNCTION__,
             m_strComPort);
 
-  // scanning the CEC bus takes about 5 seconds, so display a notification to inform users that
-  // we're busy
-  std::string strMessage =
-      StringUtils::Format(g_localizeStrings.Get(21336), g_localizeStrings.Get(36000));
-  CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, g_localizeStrings.Get(36000),
-                                        strMessage);
-
   bool bConnectionFailedDisplayed(false);
 
   while (!m_bStop && !bIsOpen)
@@ -724,7 +717,6 @@ void CPeripheralCecAdapter::CecAlert(void* cbParam,
       break;
     case CEC_ALERT_CONNECTION_LOST:
       bReopenConnection = true;
-      iAlertString = 36030;
       break;
 #if defined(CEC_ALERT_PERMISSION_ERROR)
     case CEC_ALERT_PERMISSION_ERROR:
@@ -1634,10 +1626,6 @@ bool CPeripheralCecAdapterUpdateThread::SetInitialConfiguration(void)
 
   m_adapter->m_bIsReady = true;
 
-  // and let the gui know that we're done
-  CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, g_localizeStrings.Get(36000),
-                                        strNotification);
-
   std::unique_lock lock(m_critSection);
   m_bIsUpdating = false;
   return true;
@@ -1682,9 +1670,6 @@ void CPeripheralCecAdapterUpdateThread::Process(void)
         UpdateMenuLanguage();
         UpdateAudioSystemStatus();
       }
-
-      CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, g_localizeStrings.Get(36000),
-                                            g_localizeStrings.Get(bConfigSet ? 36023 : 36024));
 
       {
         std::unique_lock lock(m_critSection);
